ea4d4888b45d591c1ded974ae6d87192a9b4c13c
Magiccat8680 <declan.j.martino@gmail.com>
2026-01-22 00:29:53 +0000

    Remove white border from thumbnails and prevent game cards from squashing

--- vibevault-react/components/GameCard.tsx
+++ vibevault-react/components/GameCard.tsx
@@ -34,7 +34,7 @@ const GameCard: React.FC<GameCardProps> = ({ game, onLaunch, onDelete, onDragSta
     <div 
       draggable
       onDragStart={onDragStart}
-      className="group relative flex flex-col rounded-sm overflow-hidden bg-[#1b2838] border border-white/5 hover:border-[#66c0f4] hover:-translate-y-1 hover:shadow-[0_10px_20px_rgba(0,0,0,0.5)] transition-all duration-300 w-80 h-96 cursor-grab active:cursor-grabbing"
+      className="group relative flex flex-col rounded-sm overflow-hidden bg-[#1b2838] border border-white/5 hover:border-[#66c0f4] hover:-translate-y-1 hover:shadow-[0_10px_20px_rgba(0,0,0,0.5)] transition-all duration-300 w-80 h-96 flex-shrink-0 cursor-grab active:cursor-grabbing"
     >
       
       {/* --- DELETE BUTTON (Absolute Overlay - Highest Z-Index) --- */}
@@ -55,7 +55,7 @@ const GameCard: React.FC<GameCardProps> = ({ game, onLaunch, onDelete, onDragSta
         {/* Background Visuals */}
         <div className="absolute inset-0 pointer-events-none select-none">
              <div className="absolute inset-0" style={theme} />
-             <div className="w-full h-full bg-white opacity-95 flex items-center justify-center overflow-hidden">
+             <div className="w-full h-full bg-black flex items-center justify-center overflow-hidden">
                 <div style={{ 
                   width: '320px', 
                   height: '240px',
